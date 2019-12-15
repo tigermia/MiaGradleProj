cd2eb3be351c5f23111ee80b1b479a82af1dc7a5
tigermia <tigermia888@gmail.com>
2019-12-14 22:27:50 -0500

    change text

--- src/main/java/HelloGradle.java
+++ src/main/java/HelloGradle.java
@@ -1,6 +1,6 @@
 public class HelloGradle {
     public static void main(String[] args) {
         System.out.println("Hello Gradle Gradle Gradle!!!!");
-        System.out.println("Hello Gradle Gradle Gradle!!!!");
+        System.out.println("Hello Master Gradle Gradle!!!!");
     }
 }
